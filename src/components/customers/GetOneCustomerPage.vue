368bff29f3a0b0991b1de95ff4f0dc2c063c41cf
Atilola Oluwatobiloba <37556289+iamatila@users.noreply.github.com>
2024-06-03 16:29:59 +0100

    Add files via upload

--- src/components/customers/GetOneCustomerPage.vue
+++ src/components/customers/GetOneCustomerPage.vue
@@ -7,10 +7,10 @@
         <q-form @submit="onSubmit" @reset="onReset" class="row">
           <div class="col-8 q-pl-md">
             <q-input
-              label="Customer Phone Number Or Customer ID"
+              label="Last Name Or Customer Phone Number Or Customer ID"
               class=""
               outlined
-              v-model="phone1_customer_id"
+              v-model="phone1_customerid_lastname"
               hint=""
             />
           </div>
@@ -37,76 +37,90 @@
       </div> -->
 
       <div class="q-pa-md row items-start q-gutter-md" v-if="fixed0">
-        <q-card class="my-card-2 bg-primary text-white">
+        <q-card
+          v-for="(da, index) in data"
+          :key="index"
+          class="my-card-2 text-primary q-mt-md"
+        >
           <q-card-section>
             <div class="row q-pt-sm">
               <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Created At</div>
-                <div class="text-h6">{{ formatDate(data.CreatedAt) }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">
+                  Created At
+                </div>
+                <div class="text-h6">{{ formatDate(da.CreatedAt) }}</div>
               </div>
               <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Customer ID</div>
-                <div class="text-h6">{{ data.customer_id }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">
+                  Customer ID
+                </div>
+                <div class="text-h6">{{ da.customer_id }}</div>
               </div>
             </div>
             <div class="row q-pt-sm">
               <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">First Name</div>
-                <div class="text-h6">{{ data.first_name }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">
+                  First Name
+                </div>
+                <div class="text-h6">{{ da.first_name }}</div>
               </div>
               <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Last Name</div>
-                <div class="text-h6">{{ data.last_name }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">Last Name</div>
+                <div class="text-h6">{{ da.last_name }}</div>
               </div>
             </div>
             <div class="row q-pt-sm">
               <div class="col-12 col-md-12 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Email</div>
-                <div class="text-h6">{{ data.email }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">Email</div>
+                <div class="text-h6">{{ da.email }}</div>
               </div>
               <!-- <div class="col-12 col-md-6 q-pl-md">
-                    <div class="text-h6">{{ data.username }}</div>
+                    <div class="text-h6">{{ da.username }}</div>
                     <div class="text-subtitle2">Username</div>
                   </div> -->
               <!-- <div class="col-12 col-md-6 q-pl-md">
-                    <div class="text-subtitle2 bg-grey">Customer Type</div>
-                    <div class="text-h6">{{ data.user_type }}</div>
+                    <div class="text-subtitle2 bg-teal-9 text-white">Customer Type</div>
+                    <div class="text-h6">{{ da.user_type }}</div>
                   </div> -->
             </div>
             <div class="row q-pt-sm">
               <div class="col-12 col-md-12 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Phone Number</div>
-                <div class="text-h6">{{ data.phone1 }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">
+                  Phone Number
+                </div>
+                <div class="text-h6">{{ da.phone1 }}</div>
               </div>
               <!-- <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Phone Number 2</div>
-                <div class="text-h6">{{ data.phone2 }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">Phone Number 2</div>
+                <div class="text-h6">{{ da.phone2 }}</div>
               </div> -->
             </div>
             <div class="row q-pt-sm">
               <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Gender</div>
-                <div class="text-h6">{{ data.gender }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">Gender</div>
+                <div class="text-h6">{{ da.gender }}</div>
               </div>
               <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Date of Birth</div>
-                <div class="text-h6">{{ data.dob }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">
+                  Date of Birth
+                </div>
+                <div class="text-h6">{{ da.dob }}</div>
               </div>
             </div>
             <div class="row q-pt-sm">
               <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">State</div>
-                <div class="text-h6">{{ data.state }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">State</div>
+                <div class="text-h6">{{ da.state }}</div>
               </div>
               <div class="col-12 col-md-6 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Country</div>
-                <div class="text-h6">{{ data.country }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">Country</div>
+                <div class="text-h6">{{ da.country }}</div>
               </div>
             </div>
             <div class="row q-pt-sm">
               <div class="col-12 col-md-12 q-pl-md">
-                <div class="text-subtitle2 bg-grey">Address</div>
-                <div class="text-h6">{{ data.address }}</div>
+                <div class="text-subtitle2 bg-teal-9 text-white">Address</div>
+                <div class="text-h6">{{ da.address }}</div>
               </div>
             </div>
           </q-card-section>
@@ -115,10 +129,8 @@
 
           <q-card-actions align="right">
             <q-btn
-              @click="
-                goToCreateIntake(data.first_name, data.last_name, data.phone1)
-              "
-              flat
+              class="bg-teal-9 text-white"
+              @click="goToCreateIntake(da.first_name, da.last_name, da.phone1)"
               >Create Intake</q-btn
             >
           </q-card-actions>
@@ -141,7 +153,7 @@ const fixed0 = ref(false);
 const fixed1 = ref(false);
 const $q = useQuasar();
 const data = ref([]);
-const phone1_customer_id = ref("");
+const phone1_customerid_lastname = ref("");
 const phone1 = ref("");
 const customer_id = ref("");
 const useStore = useUserStore();
@@ -190,13 +202,10 @@ const goToCreateIntake = (first_name, last_name, phone) => {
 };
 
 const onSubmit = () => {
-  // const formData = {
-  //   customer_id: customer_id.value,
-  // }
   const token = useStore.getToken;
 
   axios
-    .get(`${base}/customer/${phone1_customer_id.value}`, {
+    .get(`${base}/customer/${phone1_customerid_lastname.value}`, {
       headers: { Authorization: `Bearer ${token}` },
     })
     .then((response) => {
@@ -254,7 +263,7 @@ const onSubmit = () => {
 const onReset = () => {
   phone1.value = "";
   customer_id.value = "";
-  phone1_customer_id.value = "";
+  phone1_customerid_lastname.value = "";
 };
 </script>
 
